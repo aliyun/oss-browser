3cb5614df74c37e95399b6afdf536511329feaeb
guangchun.luo <guangchun.luo@alibaba-inc.com>
2017-07-03 16:02:59 +0800

    fix crc64 test case

--- node/crc64/test/index.js
+++ node/crc64/test/index.js
@@ -1,4 +1,4 @@
-var crc64 = require('../crc64.js')
+var CRC64 = require('../crc64.js')
 var path = require('path')
 var fs = require("fs")
 const assert = require('assert');
@@ -6,33 +6,33 @@ const assert = require('assert');
 const result1 = '11051210869376104954';
 const result2 = '5178350320981835788';
 
-var r1 = crc64.check("123456789");
+var r1 = new CRC64().check("123456789");
 console.log(r1);
 assert(r1 === result1);
 
-var r2 = crc64.check(0,"123456789");
+var r2 = new CRC64().check(0,"123456789");
 console.log(r2);
 assert(r2 === result1);
 
-var r3 = crc64.check("0","123456789");
+var r3 = new CRC64().check("0","123456789");
 console.log(r3);
 assert(r3 === result1);
 
-var r4 = crc64.check(Buffer.from("123456789"))
+var r4 = new CRC64().check(Buffer.from("123456789"))
 console.log(r4);
 assert(r4 === result1);
 
-var r5 = crc64.check(0,Buffer.from("123456789"))
+var r5 = new CRC64().check(0,Buffer.from("123456789"))
 console.log(r5);
 assert(r5 === result1);
 
-var r6 = crc64.check("0",Buffer.from("123456789"));
+var r6 = new CRC64().check("0",Buffer.from("123456789"));
 console.log(r6);
 assert(r6 === result1)
 
 var readStream = fs.createReadStream(path.join(__dirname,'apps.png'));
 
-crc64.check_stream(readStream, (err, result)=>{
+new CRC64().check_stream(readStream, (err, result)=>{
 	if(!err){
     console.log(result)
     assert(result === result2)
