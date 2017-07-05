de5d3bc96b3776c4235e803ca4a940bccdea3784
guangchun.luo <guangchun.luo@alibaba-inc.com>
2017-07-05 21:22:56 +0800

    优化 crc64 获取校验码方法

--- node/ossstore/lib/util.js
+++ node/ossstore/lib/util.js
@@ -14,8 +14,52 @@ module.exports = {
   getFileCrc64: getFileCrc64
 };
 
-
 function getFileCrc64(p, fn){
+  if(!CRC64){
+    console.warn('not found crc64 module');
+    fn(null, null);
+    return;
+  }
+
+  var arr = [];
+  var c = 0;
+  _dig();
+
+  //至少获取 2 遍，如果有2个一样的，则返回
+  function _dig(){
+    console.time('get crc64 hash for ['+p+']');
+    var stream = fs.createReadStream(p);
+    CRC64.check_stream(stream, function(err, data){
+      stream.close();
+      console.timeEnd('get crc64 hash for ['+p+']');
+
+      if(err)fn(err);
+      else{
+         arr[c]=data;
+         c++;
+         if(c>1){
+           var m={};
+           for(var i=0;i<arr.length;i++){
+             if(!m[arr[i]]){
+               m[arr[i]]=1;
+             }
+             else{
+               if(arr.length>2) console.error('crc64 hash for ['+p+']', arr);
+               //else console.info('crc64 hash for ['+p+']', arr);
+               fn(null, data);
+               return;
+             }
+           }
+         }else{
+           _dig();
+         }
+      }
+    });
+  }
+};
+
+
+function getFileCrc64_2(p, fn){
   if(!CRC64){
     console.warn('not found crc64 module');
     fn(null, null);
