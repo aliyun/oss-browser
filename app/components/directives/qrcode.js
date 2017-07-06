4112e8bf09b8c47cd518a52fc3e06f31f1071875
guangchun.luo <guangchun.luo@alibaba-inc.com>
2017-07-07 05:36:49 +0800

    增加扫码下载支持

---
+++ app/components/directives/qrcode.js
@@ -0,0 +1,36 @@
+angular.module('web')
+  .directive('qrcode', ['$timeout', function ($timeout) {
+
+    return {
+      link: linkFn,
+      restrict: 'EA',
+      transclude: true,
+      template: '<div class="qrcode"><div></div></div><ng-transclude></ng-transclude>',
+      scope: {
+        text: '=',
+        width: '=',
+        height: '=',
+        label: '='
+      }
+    };
+
+    function linkFn(scope, ele, attr) {
+
+      scope.$watch('text', function(v){
+        reset(v)
+      });
+
+      function reset(v){
+        var w = scope.width || scope.height || 100;
+        var h = scope.height || scope.width || 100;
+        $(ele).find('.qrcode').html('<div></div>');
+        if(v)
+        $($(ele).find('.qrcode>div')).qrcode({
+          text: v||'',
+          width: w,
+          height: h
+        });
+      }
+    }
+
+  }]);
--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ gulp.task('libJS', function () {
 
   var arr=[
       './node_modules/jquery/dist/jquery.js',
+      './node_modules/jquery.qrcode/jquery.qrcode.min.js',
 
       './node_modules/moment/min/moment-with-locales.js',
       './node_modules/bootstrap/dist/js/bootstrap.js',
