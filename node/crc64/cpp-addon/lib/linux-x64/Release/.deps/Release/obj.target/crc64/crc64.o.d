ccfb49ea634bb9847d7154772b21deaadc54c865
aleelock <aleelock@126.com>
2017-11-19 11:46:26 +0800

    update crc64 for linuxx64

--- node/crc64/cpp-addon/lib/linux-x64/Release/.deps/Release/obj.target/crc64/crc64.o.d
+++ node/crc64/cpp-addon/lib/linux-x64/Release/.deps/Release/obj.target/crc64/crc64.o.d
@@ -1,17 +1,17 @@
-cmd_Release/obj.target/crc64/crc64.o := g++ '-DNODE_GYP_MODULE_NAME=crc64' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/include/node -I/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src -I/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/uv/include -I/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/crc64/crc64.o.d.raw   -c -o Release/obj.target/crc64/crc64.o ../crc64.cc
+cmd_Release/obj.target/crc64/crc64.o := g++ '-DNODE_GYP_MODULE_NAME=crc64' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/include/node -I/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src -I/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/uv/include -I/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/crc64/crc64.o.d.raw   -c -o Release/obj.target/crc64/crc64.o ../crc64.cc
 Release/obj.target/crc64/crc64.o: ../crc64.cc \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node.h \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include/v8.h \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include/v8-version.h \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include/v8config.h \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node_version.h \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node_buffer.h \
- /home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node.h
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node.h \
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include/v8.h \
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include/v8-version.h \
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include/v8config.h \
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node_version.h \
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node_buffer.h \
+ /home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node.h
 ../crc64.cc:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node.h:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include/v8.h:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include/v8-version.h:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/deps/v8/include/v8config.h:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node_version.h:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node_buffer.h:
-/home/zub/.electron-gyp/.node-gyp/iojs-1.7.9/src/node.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include/v8.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include/v8-version.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/deps/v8/include/v8config.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node_version.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node_buffer.h:
+/home/zub/.electron-gyp/.node-gyp/iojs-1.6.5/src/node.h:
