20ffa47a1ad1928a94ff12b407e2772f655c76ad
润生 <sdlcdhch@gmail.com>
2018-08-27 18:47:29 +0800

    删除单个文件时调用delete接口，删除多个文件时再调用deleteMulti接口

--- app/components/services/oss2.js
+++ app/components/services/oss2.js
@@ -210,18 +210,33 @@ angular.module('web')
               objectsCount++;
 
               if (itemsToDelete.length == 500 || (objectsCount!=0 && objectsCount + foldersCount == len)) {
-                client.deleteMulti(itemsToDelete).then(function (res) {
-                  c += itemsToDelete.length;
-                  progress.current += itemsToDelete.length;
-                  itemsToDelete.splice(0, itemsToDelete.length);
-                  $timeout(dig,NEXT_TICK);
-                }).catch(function (err) {
-                  terr.push({item:item, error:err});
-                  progress.errorCount += itemsToDelete.length;
-                  c += itemsToDelete.length;
-                  itemsToDelete.splice(0, itemsToDelete.length);
-                  $timeout(dig,NEXT_TICK);
-                });
+                if (itemsToDelete.length > 1) {
+                  client.deleteMulti(itemsToDelete).then(function (res) {
+                    c += itemsToDelete.length;
+                    progress.current += itemsToDelete.length;
+                    itemsToDelete.splice(0, itemsToDelete.length);
+                    $timeout(dig,NEXT_TICK);
+                  }).catch(function (err) {
+                    terr.push({item:item, error:err});
+                    progress.errorCount += itemsToDelete.length;
+                    c += itemsToDelete.length;
+                    itemsToDelete.splice(0, itemsToDelete.length);
+                    $timeout(dig,NEXT_TICK);
+                  });
+                } else {
+                  client.delete(itemsToDelete[0]).then(function (res) {
+                    c += itemsToDelete.length;
+                    progress.current += itemsToDelete.length;
+                    itemsToDelete.splice(0, itemsToDelete.length);
+                    $timeout(dig,NEXT_TICK);
+                  }).catch(function (err) {
+                    terr.push({item:item, error:err});
+                    progress.errorCount += itemsToDelete.length;
+                    c += itemsToDelete.length;
+                    itemsToDelete.splice(0, itemsToDelete.length);
+                    $timeout(dig,NEXT_TICK);
+                  });
+                }
               } else {
                 $timeout(dig,NEXT_TICK);
               }
