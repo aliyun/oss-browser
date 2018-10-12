f4ea79e959910e4ece438fc9009c550f608ccd2e
罗章 <luozhang002@126.com>
2018-10-12 11:44:45 +0800

    fix: keep same code style with es5 between main process and renderer process

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-const electron = require('electron');
+var electron = require('electron');
 // Module to control application life.
-const {
-  app,
-  Menu,
-  ipcMain,
-  BrowserWindow
-} = electron;
 
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
-const nativeImage = require('electron').nativeImage;
+var app = electron.app;
+var Menu = electron.Menu;
+var ipcMain = electron.ipcMain;
+var BrowserWindow = electron.BrowserWindow;
+
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var nativeImage = require('electron').nativeImage;
 
 ///*****************************************
 //静态服务
-const PORTS = [7123,7124,7125,7126];
+var PORTS = [7123,7124,7125,7126];
 
 for(var port of PORTS){
   try{
@@ -30,8 +29,8 @@ for(var port of PORTS){
 app.commandLine.appendSwitch('ignore-certificate-errors')
 ///*****************************************
 
-
 var custom={};
+
 try{
   custom = require(path.join(__dirname,'../custom'));
 }catch(e){
@@ -41,7 +40,7 @@ try{
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+var win;
 
 if (process.platform == 'darwin') {
   app.dock.setIcon(custom.logo_png || path.join(__dirname, 'icons', 'icon.png'));
@@ -91,7 +90,7 @@ function createWindow() {
 
     if (process.platform === 'darwin') {
       // Create the Application's main menu
-      let template = getMenuTemplate();
+      var template = getMenuTemplate();
       //注册菜单, 打包后可以复制, 但是不能打开 devTools
       Menu.setApplicationMenu(Menu.buildFromTemplate(template));
     }
@@ -167,7 +166,7 @@ function moveFile(from, to, fn){
 }
 
 //singleton
-const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+var shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
   // Someone tried to run a second instance, we should focus our window.
   if (win) {
     if (win.isMinimized()) win.restore();
@@ -208,7 +207,7 @@ app.on('activate', () => {
 // code. You can also put them in separate files and require them here.
 
 function getMenuTemplate() {
-  const name = app.getName();
+  var name = app.getName();
   return [{
     label: name,
     submenu: [{
