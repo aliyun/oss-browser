250c807b2698a6ed3fd557ce0d730960c25ff031
罗章 <luozhang002@126.com>
2019-01-07 16:23:49 +0800

    chore: eslint

--- node/ossstore/lib/index.js
+++ node/ossstore/lib/index.js
@@ -1,13 +1,12 @@
-
 'use strict';
 var ALYD = require('aliyun-sdk');
 require('events').EventEmitter.prototype._maxListeners = 1000;
 // var TIMEOUT = 30000; //30秒
-var TIMEOUT = parseInt(localStorage.getItem('connectTimeout')||60000); //30秒
-console.log("TIMEOUT: "+ TIMEOUT)
+var TIMEOUT = parseInt(localStorage.getItem('connectTimeout') || 60000); //30秒
+console.log("TIMEOUT: " + TIMEOUT)
 //fix
-ALYD.util.isBrowser = function(){
-    return false;
+ALYD.util.isBrowser = function () {
+  return false;
 };
 
 
@@ -27,82 +26,81 @@ var DownloadJob = require('./download-job');
  */
 
 function OssStore(config) {
-    if (!config) {
-        console.log('需要 config');
-        return;
-    }
-    this._config = {};
-    Object.assign(this._config, config);
+  if (!config) {
+    console.log('需要 config');
+    return;
+  }
+  this._config = {};
+  Object.assign(this._config, config);
 
-    if (!this._config.aliyunCredential && !this._config.stsToken) {
-        console.log('需要 stsToken');
-        return;
-    }
+  if (!this._config.aliyunCredential && !this._config.stsToken) {
+    console.log('需要 stsToken');
+    return;
+  }
 
-    if (!this._config.endpoint) {
-        console.log('需要 endpoint');
-        return;
-    }
+  if (!this._config.endpoint) {
+    console.log('需要 endpoint');
+    return;
+  }
 
 
+  if (this._config.stsToken) {
+    this.oss = new ALYD.OSS({
+      accessKeyId: this._config.stsToken.Credentials.AccessKeyId,
+      secretAccessKey: this._config.stsToken.Credentials.AccessKeySecret,
+      securityToken: this._config.stsToken.Credentials.SecurityToken,
+      endpoint: this._config.endpoint,
+      apiVersion: '2013-10-15',
+      maxRetries: 0,
+      httpOptions: {
+        timeout: TIMEOUT
+      },
+      cname: this._config.cname,
+      isRequestPayer: localStorage.getItem("show-request-pay") === 'YES' ? true : false
+    });
+  }
+  else {
+    this.oss = new ALYD.OSS({
+      accessKeyId: this._config.aliyunCredential.accessKeyId,
+      secretAccessKey: this._config.aliyunCredential.secretAccessKey,
+      endpoint: this._config.endpoint,
+      apiVersion: '2013-10-15',
+      maxRetries: 0,
+      httpOptions: {
+        timeout: TIMEOUT
+      },
+      cname: this._config.cname,
+      isRequestPayer: localStorage.getItem("show-request-pay") === 'YES' ? true : false
+    });
+  }
 
-    if (this._config.stsToken) {
-        this.oss = new ALYD.OSS({
-            accessKeyId: this._config.stsToken.Credentials.AccessKeyId,
-            secretAccessKey: this._config.stsToken.Credentials.AccessKeySecret,
-            securityToken: this._config.stsToken.Credentials.SecurityToken,
-            endpoint: this._config.endpoint,
-            apiVersion: '2013-10-15',
-            maxRetries: 0,
-            httpOptions: {
-                timeout: TIMEOUT
-            },
-            cname: this._config.cname,
-            isRequestPayer: localStorage.getItem("show-request-pay") === 'YES' ? true: false
-        });
-    }
-    else {
-        this.oss = new ALYD.OSS({
-            accessKeyId: this._config.aliyunCredential.accessKeyId,
-            secretAccessKey: this._config.aliyunCredential.secretAccessKey,
-            endpoint: this._config.endpoint,
-            apiVersion: '2013-10-15',
-            maxRetries: 0,
-            httpOptions: {
-                timeout: TIMEOUT
-            },
-            cname: this._config.cname,
-            isRequestPayer: localStorage.getItem("show-request-pay") === 'YES' ? true: false
-        });
-    }
-
-    var arr = this._config.endpoint.split('://');
-    if (arr.length < 2) {
-        console.log('endpoint 格式错误');
-        return;
-    }
-    this._config.endpoint = {
-        protocol: arr[0],
-        host: arr[1]
-    };
+  var arr = this._config.endpoint.split('://');
+  if (arr.length < 2) {
+    console.log('endpoint 格式错误');
+    return;
+  }
+  this._config.endpoint = {
+    protocol: arr[0],
+    host: arr[1]
+  };
 }
 
-OssStore.prototype.setStsToken = function(stsToken){
-    this._config.stsToken = stsToken;
+OssStore.prototype.setStsToken = function (stsToken) {
+  this._config.stsToken = stsToken;
 
-    this.oss = new ALYD.OSS({
-        accessKeyId: this._config.stsToken.Credentials.AccessKeyId,
-        secretAccessKey: this._config.stsToken.Credentials.AccessKeySecret,
-        securityToken: this._config.stsToken.Credentials.SecurityToken,
-        endpoint: this._config.endpoint,
-        apiVersion: '2013-10-15',
-        maxRetries: 0,
-        httpOptions: {
-            timeout: TIMEOUT
-        },
-        cname: this._config.cname,
-        isRequestPayer: localStorage.getItem("show-request-pay") === 'YES' ? true: false
-    });
+  this.oss = new ALYD.OSS({
+    accessKeyId: this._config.stsToken.Credentials.AccessKeyId,
+    secretAccessKey: this._config.stsToken.Credentials.AccessKeySecret,
+    securityToken: this._config.stsToken.Credentials.SecurityToken,
+    endpoint: this._config.endpoint,
+    apiVersion: '2013-10-15',
+    maxRetries: 0,
+    httpOptions: {
+      timeout: TIMEOUT
+    },
+    cname: this._config.cname,
+    isRequestPayer: localStorage.getItem("show-request-pay") === 'YES' ? true : false
+  });
 };
 
 
@@ -133,12 +131,12 @@ OssStore.prototype.setStsToken = function(stsToken){
  */
 OssStore.prototype.createUploadJob = function createUploadJob(options) {
 
-    var self = this;
+  var self = this;
 
-    var job = new UploadJob(self.oss, options);
+  var job = new UploadJob(self.oss, options);
 
-    //默认是 waiting 状态
-    return job;
+  //默认是 waiting 状态
+  return job;
 };
 
 
@@ -170,13 +168,13 @@ OssStore.prototype.createUploadJob = function createUploadJob(options) {
  */
 OssStore.prototype.createDownloadJob = function createDownloadJob(options) {
 
-    var self = this;
+  var self = this;
 
-    var job = new DownloadJob(self.oss, options);
+  var job = new DownloadJob(self.oss, options);
 
-    //默认是 waiting 状态
+  //默认是 waiting 状态
 
-    return job;
+  return job;
 };
 
 
