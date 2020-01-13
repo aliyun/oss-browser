f1370fe0ffcc367231f9dd96b2b2aca39a38a7bf
PeterRao <peizerao@gmail.com>
2020-01-13 17:27:11 +0800

    feat: update electorn to 7.1.8

--- main.js
+++ main.js
@@ -61,7 +61,8 @@ function createWindow() {
     icon: custom.logo_ico || path.join(__dirname, 'icons', 'icon.ico'),
 
     webPreferences: {
-      plugins: true
+      plugins: true,
+      nodeIntegration: true
     }
   };
 
@@ -179,23 +180,37 @@ function moveFile(from, to, fn){
 }
 
 //singleton
-var shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
-  // Someone tried to run a second instance, we should focus our window.
-  if (win) {
-    if (win.isMinimized()) win.restore();
-    win.focus();
-  }
-});
+// var shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+//   // Someone tried to run a second instance, we should focus our window.
+//   if (win) {
+//     if (win.isMinimized()) win.restore();
+//     win.focus();
+//   }
+// });
+//
+// if (shouldQuit) {
+//   app.quit();
+//   process.exit(0);
+// }
 
-if (shouldQuit) {
-  app.quit();
-  process.exit(0);
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (win) {
+      if (win.isMinimized()) win.restore()
+      win.focus()
+    }
+  })
+  app.on('ready', createWindow);
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+// app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
