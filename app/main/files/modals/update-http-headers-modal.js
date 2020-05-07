5fd8877ecfdb7ce624400562fd996a3f159c060d
beajer <919060679@qq.com>
2020-05-07 14:15:00 +0800

    feat: 默认方式登录时，下载地址可选自有域名 (#304)

--- app/main/files/modals/update-http-headers-modal.js
+++ app/main/files/modals/update-http-headers-modal.js
@@ -1,73 +1,101 @@
-angular.module('web')
-  .controller('updateHttpHeadersModalCtrl', ['$scope', '$q','$translate', '$uibModalInstance', 'item', 'currentInfo', 'ossSvs2', 'Toast','safeApply',
-    function ($scope, $q, $translate, $modalInstance, item, currentInfo, ossSvs2, Toast ,safeApply) {
+angular.module("web").controller("updateHttpHeadersModalCtrl", [
+  "$scope",
+  "$q",
+  "$translate",
+  "$uibModalInstance",
+  "item",
+  "currentInfo",
+  "ossSvs2",
+  "Toast",
+  "safeApply",
+  function (
+    $scope,
+    $q,
+    $translate,
+    $modalInstance,
+    item,
+    currentInfo,
+    ossSvs2,
+    Toast,
+    safeApply
+  ) {
+    var T = $translate.instant;
+    angular.extend($scope, {
+      item: item,
+      currentInfo: currentInfo,
+      //metas: {},
+      headers: {},
+      metaItems: [],
+      cancel: cancel,
+      onSubmit: onSubmit,
+    });
 
-      var T = $translate.instant;
-      angular.extend($scope, {
-        item: item,
-        currentInfo: currentInfo,
-        //metas: {},
-        headers: {},
-        metaItems: [],
-        cancel: cancel,
-        onSubmit: onSubmit
-      });
+    function cancel() {
+      $modalInstance.dismiss("close");
+    }
 
-      function cancel() {
-        $modalInstance.dismiss('close');
-      }
-
-      init();
-      function init(){
-        $scope.isLoading = true;
-        $scope.step=2;
-        var ignoreError = true;
-        if(item.length > 1) return;
-        ossSvs2.getMeta(currentInfo.region, currentInfo.bucket, item[0].path).then(function(result){
+    init();
+    function init() {
+      $scope.isLoading = true;
+      $scope.step = 2;
+      var ignoreError = true;
+      if (item.length > 1) return;
+      ossSvs2
+        .getMeta(currentInfo.region, currentInfo.bucket, item[0].path)
+        .then(function (result) {
           $scope.headers = {
-            'ContentLanguage': result.ContentLanguage,
-            'ContentType': result.ContentType,
-            'CacheControl': result.CacheControl,
-            'ContentDisposition': result.ContentDisposition,
-            'ContentEncoding': result.ContentEncoding,
-            'Expires': result.Expires,
+            ContentLanguage: result.ContentLanguage,
+            ContentType: result.ContentType,
+            CacheControl: result.CacheControl,
+            ContentDisposition: result.ContentDisposition,
+            ContentEncoding: result.ContentEncoding,
+            Expires: result.Expires,
           };
           //$scope.metas = result.Metadata;
 
-          var t=[];
-          for(var k in result.Metadata){
-            t.push({key: k, value: result.Metadata[k]});
+          var t = [];
+          for (var k in result.Metadata) {
+            t.push({ key: k, value: result.Metadata[k] });
           }
           $scope.metaItems = t;
 
           safeApply($scope);
-
         });
+    }
 
+    function onSubmit(form1) {
+      if (!form1.$valid) return;
+
+      var headers = angular.copy($scope.headers);
+      var metaItems = angular.copy($scope.metaItems);
+
+      for (var k in headers) {
+        if (!headers[k]) delete headers[k];
       }
 
-      function onSubmit(form1){
-        if(!form1.$valid)return;
-
-        var headers = angular.copy($scope.headers);
-        var metaItems = angular.copy($scope.metaItems);
-
-        for(var k in headers){
-          if(!headers[k])delete headers[k];
-        }
-
-        var metas = {};
-        angular.forEach(metaItems,function(n){
-          if(n.key && n.value)metas[n.key]=n.value;
-        });
-        //console.log(headers, metas)
-        Toast.info(T('setting.on')); //'正在设置..'
-        Promise.all(item.map(i => ossSvs2.setMeta2(currentInfo.region, currentInfo.bucket, i.path, headers, metas))).then(result => {
-          Toast.success(T('setting.success')); //'设置成功'
-        }).finally(() => {
+      var metas = {};
+      angular.forEach(metaItems, function (n) {
+        if (n.key && n.value) metas[n.key] = n.value;
+      });
+      //console.log(headers, metas)
+      Toast.info(T("setting.on")); //'正在设置..'
+      Promise.all(
+        item.map((i) =>
+          ossSvs2.setMeta2(
+            currentInfo.region,
+            currentInfo.bucket,
+            i.path,
+            headers,
+            metas
+          )
+        )
+      )
+        .then((result) => {
+          Toast.success(T("setting.success")); //'设置成功'
+        })
+        .finally(() => {
           cancel();
         });
-      }
-
     }
-  ]);
+  },
+]);
