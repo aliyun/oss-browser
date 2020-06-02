ac20d109124f4d714c81ac949723ee2427810f5a
beajer <919060679@qq.com>
2020-06-02 16:46:15 +0800

    feat: 请求错误时提示requestId且复制 (#322)

--- app/components/directives/toast-list.js
+++ app/components/directives/toast-list.js
@@ -14,15 +14,23 @@ angular
     return {
       //link: linkFn,
       restrict: "EA",
-      template:
-        '<div class="toast-list" style="position: fixed; bottom: 0px;left: 10px;right: 70%;z-index:10000;">' +
-        '<div ng-repeat="alert in alerts" style="padding:4px;margin-bottom:10px;" class="break alert alert-{{alert.type}}">{{alert.msg}}</div>' +
-        "</div>",
+      template: `
+        <div class="toast-list" style="position: fixed; bottom: 0px;left: 10px;right: 70%;z-index:10000;">
+          <div ng-repeat="alert in alerts" style="padding:4px;margin-bottom:10px;" class="break alert alert-{{alert.type}}">
+            <p style="margin: 0">{{alert.msg}}<p>
+            <p ng-if="alert.requestId" style="margin: 0">
+              RequestId: {{alert.requestId}}
+              <button ng-click="copyClip(alert.requestId)">复制</button>
+            <p>
+          </div>
+        </div>`,
       controller: [
         "$scope",
         "$timeout",
         "$location",
-        function ($scope, $timeout, $location) {
+        function ($scope, $timeout) {
+          const { clipboard } = require("electron");
+
           $scope.alerts = [];
 
           // $scope.closeAlert = function(index){
@@ -30,14 +38,24 @@ angular
           // };
 
           $scope.$on("message", function (evt, data) {
-            showMessage(data.message, data.type || "danger", data.ttl || 3000);
+            showMessage(
+              data.message,
+              data.type || "danger",
+              data.ttl || 3000,
+              data.requestId
+            );
           });
 
-          function showMessage(msg, type, ttl) {
+          $scope.copyClip = function (text) {
+            clipboard.writeText(text);
+          };
+
+          function showMessage(msg, type, ttl, requestId) {
             var obj = {
               type: type || "danger",
               msg: msg || "",
               id: Math.random(),
+              requestId: requestId,
             };
 
             //next tick
@@ -56,8 +74,6 @@ angular
         },
       ],
     };
-
-    function linkFn(scope, ele, attr) {}
   })
 
   .factory("Toast", [
@@ -76,16 +92,17 @@ angular
         warning: function (msg, ttl) {
           sendMessage(msg, "warning", ttl);
         },
-        error: function (msg, ttl) {
-          sendMessage(msg, "danger", ttl);
+        error: function (msg, ttl, requestId) {
+          sendMessage(msg, "danger", ttl, requestId);
         },
       };
 
-      function sendMessage(msg, type, ttl) {
+      function sendMessage(msg, type, ttl, requestId) {
         $rootScope.$broadcast("message", {
           message: msg,
           type: type,
           ttl: ttl,
+          requestId: requestId,
         });
       }
     },
