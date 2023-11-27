2eeb97224b0926cc0184dc4607b23a71e18598da
csg01123119 <csg01123119@alibaba-inc.com>
2023-11-27 17:29:10 +0800

    feat: add authorization code display

--- app/main/files/modals/grant-modal-code.js
+++ app/main/files/modals/grant-modal-code.js
@@ -3,23 +3,33 @@ angular.module("web").controller("grantModalCtrlCode", [
   "$scope",
   "$sce",
   "$uibModalInstance",
+  "Toast",
   "info",
-  function ($translate, $scope, $sce, $modalInstance, info) {
+  function ($translate, $scope, $sce, $modalInstance, Toast, info) {
     var T = $translate.instant;
 
     const { html } = info;
-    const regx = /<code.*>.*<\/code>/gi;
+    const regx = /<code.*>(.*)<\/code>/gi;
     let ihtml = "";
-    const res = html.match(regx);
-    if (res.length > 0) ihtml = `${T("auth.tokenLogin")}:<br/><br/>${res[0]}`;
+    const res = regx.exec(html);
+    if (res.length > 1) ihtml = `${T("auth.tokenLogin")}:<br/><br/>${res[0]}`;
 
     angular.extend($scope, {
       cancel: cancel,
+      copy: copy,
       htm: $sce.trustAsHtml(ihtml),
     });
 
     function cancel() {
       $modalInstance.dismiss("close");
+    }
+
+    function copy() {
+      const { clipboard } = require("electron");
+      if (res.length > 1) {
+        clipboard.writeText(res[1]);
+        Toast.success(T("copy.successfully"));
+      }
     }
   },
 ]);
